0b45d726c69994b61e5aefd3e37faef6f52c0daa
Lexicon <Lexicon@DESKTOP-C9S8O8J>
2016-08-08 11:14:01 +0200

    Add project files.

---
+++ Game_AVP2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Game_AVP2.Startup))]
+namespace Game_AVP2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
