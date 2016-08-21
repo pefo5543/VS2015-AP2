2e99a10b1c87689c0ad7ef3cfa9273503ad7bd0b
Petter <Petter@PETTER-PC>
2016-08-21 22:57:19 +0200

    staticc

--- Game_AVP2/Scripts/AngularModels/AdminCharacterModel.js
+++ Game_AVP2/Scripts/AngularModels/AdminCharacterModel.js
@@ -6,23 +6,29 @@ var AdminCharacterModel = angular
     .module("AdminCharacterModel", ['angular-growl'])
     .constant('Enums', {
 
-        RarityEnums: [
+        AttributeEnums: [
+    { id: 1, name: 7 },
+    { id: 2, name: 8 },
+    { id: 3, name: 9 },
+    { id: 4, name: 10 },
+    { id: 5, name: 11 },
+    { id: 6, name: 12 },
+    { id: 7, name: 13},
+    { id: 8, name: 14},
+    { id: 9, name: 15 },
+    { id: 10, name: 16 }
+        ],
+        AttributeLowEnums: [
     { id: 1, name: 1 },
     { id: 2, name: 2 },
     { id: 3, name: 3 },
     { id: 4, name: 4 },
     { id: 5, name: 5 }
         ],
-        ArmourTypeEnums: [
-    { id: 1, name: 'Leather armour' },
-    { id: 2, name: 'Iron Armour' },
-    { id: 3, name: 'Steel Armour' },
-    { id: 4, name: 'Dragon Armour' }
-        ]
     })
 .controller("StaticCharacterController", function ($scope, $location, Enums, growl, CharacterService, EditCharacterService, DeleteCharacterService, entityService) {
     $scope.detail = {};
-    //$scope.enums = Enums;
+    $scope.enums = Enums;
     //$scope.addShow = true;
     getCharacters(true, 0);
     console.log($location.path);
